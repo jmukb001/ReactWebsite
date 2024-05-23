66585941650a9432bce010dafcec177c658ebe6a
Jad Mukbel <jadmukbel@gmail.com>
2024-05-22 19:27:35 -0700

    Fixing Hero

--- src/Hero.js
+++ src/Hero.js
@@ -8,16 +8,16 @@ const Hero = () => {
       <div className="hero">
         <div className="hero-content">
           <img src="./MyPhoto.png" alt="Jad Mukbel" className="hero-photo" />
-          <h1 className="hero-name">Hello, I'm Jad Mukbel!</h1>
+          <h1 className="hero-name">Hello, I'm Jad Mukbel</h1>
           <p className="hero-description">
             Current Computer Science Undergraduate at the University of California, Riverside.
           </p>
           <div className="social-icons">
             <a href="https:/www.linkedin.com/in/jad-mukbel" target="_blank" rel="noopener noreferrer">
-              <img src="LinkedIn.png" alt="LinkedIn" className="social-icon" />
+              <img src="L./inkedIn.png" alt="LinkedIn" className="social-icon" />
             </a>
             <a href="https://github.com/jmukb001" target="_blank" rel="noopener noreferrer">
-              <img src="github.png" alt="Github" className="social-icon" />
+              <img src="./github.png" alt="Github" className="social-icon" />
             </a>
           </div>
         
